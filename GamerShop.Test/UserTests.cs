0ef2a3d3afef3a97e88a557a45c96ac2cffd91ae
Edgar Sokol <edgar.sokol@gmail.com>
2024-09-28 14:34:35 +0300

    user service full coverage test

--- GamerShop.Test/UserTests.cs
+++ GamerShop.Test/UserTests.cs
@@ -2,7 +2,6 @@ using GamerShop.Core.Contracts;
 using GamerShop.Core.Models;
 using GamerShop.Core.Services;
 using Moq;
-using System.Runtime.Intrinsics.X86;
 
 namespace GamerShop.Test
 {
@@ -14,6 +13,7 @@ namespace GamerShop.Test
             //Arrange
             Mock<IUserRepository> _usersRepository = new Mock<IUserRepository>();
             Mock<IMongoDbRepository> _usersMongoRepository = new Mock<IMongoDbRepository>();
+
 
             List<User> userList = new List<User>();
             User user1 = new User
@@ -42,19 +42,77 @@ namespace GamerShop.Test
                 Email = "Rikuda@mail.com",
                 PhoneNumber = "1564487135"
             };
+
             userList.Add(user1);
             userList.Add(buyer2);
             userList.Add(seller3);
 
-            _usersRepository.Setup(x => x.GetAllUsers().Result).Returns(userList);
-            _usersRepository.Setup(x => x.GetTotalUserCount().Result).Returns(userList.Count);
-            _usersMongoRepository.Setup(x => x.GetUserCount().Result).Returns(0);
+            _usersRepository.Setup(x => x.GetTotalUserCount()).Returns(Task.FromResult(userList.Count));
+            _usersMongoRepository.Setup(x => x.GetUserCount()).Returns(Task.FromResult(0L));
+            _usersRepository.Setup(x => x.GetAllUsers()).Returns(Task.FromResult(userList));
+            List<User>? mongo = null;
+            _usersMongoRepository.Setup(x => x.InsertUserList(It.IsAny<List<User>>())).Callback<List<User>>(userList => mongo = userList);
+
             IUserService userService = new UserService(_usersRepository.Object, _usersMongoRepository.Object);
 
             //Act
             var testList = await userService.GetAllUsers();
             //Assert
             Assert.Equal(userList.Count, testList.Count);
+            Assert.Equal(userList, testList);
+            Assert.Equal(mongo, testList);
+        }
+
+        [Fact]
+        public async Task GetUserListFromMongoDBTest()
+        {
+            //Arrange
+            Mock<IUserRepository> _usersRepository = new Mock<IUserRepository>();
+            Mock<IMongoDbRepository> _usersMongoRepository = new Mock<IMongoDbRepository>();
+
+
+            List<User> userList = new List<User>();
+            User user1 = new User
+            {
+                Id = 15,
+                FirstName = "Edgar",
+                LastName = "Jugger",
+                Email = "jugger@mail.com",
+                PhoneNumber = "15687135"
+            };
+
+            Buyer buyer2 = new Buyer
+            {
+                Id = 16,
+                FirstName = "Edgar",
+                LastName = "Juggernaut",
+                Email = "Juggernaut@mail.com",
+                PhoneNumber = "1568742135"
+            };
+
+            Seller seller3 = new Seller
+            {
+                Id = 17,
+                FirstName = "Edgar",
+                LastName = "Rikuda",
+                Email = "Rikuda@mail.com",
+                PhoneNumber = "1564487135"
+            };
+
+            userList.Add(user1);
+            userList.Add(buyer2);
+            userList.Add(seller3);
+
+            _usersRepository.Setup(x => x.GetTotalUserCount()).Returns(Task.FromResult(userList.Count));
+            _usersMongoRepository.Setup(x => x.GetUserCount()).Returns(Task.FromResult((long)userList.Count));
+            _usersMongoRepository.Setup(x => x.GetAllUsers()).Returns(Task.FromResult(userList));
+            IUserService userService = new UserService(_usersRepository.Object, _usersMongoRepository.Object);
+
+            //Act
+            var testList = await userService.GetAllUsers();
+            //Assert
+            Assert.Equal(userList.Count, testList.Count);
+            Assert.Equal(userList, testList);
         }
 
         [Fact]
@@ -123,6 +181,44 @@ namespace GamerShop.Test
             //Assert
             Assert.Equal(16, testSubject.Id);
         }
+
+        [Fact]
+        public async Task GetUserByIdUserNullTest()
+        {
+            //Arrange
+            Mock<IUserRepository> _usersRepository = new Mock<IUserRepository>();
+            Mock<IMongoDbRepository> _usersMongoRepository = new Mock<IMongoDbRepository>();
+
+            List<User> userList = new List<User>();
+            Buyer buyer2 = new Buyer
+            {
+                Id = 16,
+                FirstName = "Edgar",
+                LastName = "Juggernaut",
+                Email = "Juggernaut@mail.com",
+                PhoneNumber = "1568742135"
+            };
+
+            Seller seller3 = new Seller
+            {
+                Id = 17,
+                FirstName = "Edgar",
+                LastName = "Rikuda",
+                Email = "Rikuda@mail.com",
+                PhoneNumber = "1564487135"
+            };
+            userList.Add(buyer2);
+            userList.Add(seller3);
+
+            _usersMongoRepository.Setup(x => x.GetUserById(16).Result).Returns(buyer2);
+            IUserService userService = new UserService(_usersRepository.Object, _usersMongoRepository.Object);
+
+            //Act
+            User testSubject = await userService.GetUserById(16);
+            //Assert
+            Assert.Equal(buyer2.Id, testSubject.Id);
+        }
+
 
         [Fact]
         public async Task CreateUserTest()
